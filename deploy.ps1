cafd8e00b132b0f765ef15952c4cba8c3e4fd2af
iot91355 <iot91355@gmail.com>
2025-09-26 14:34:34 +0530

    deploy.ps1 modified arg

--- deploy.ps1
+++ deploy.ps1
@@ -4,14 +4,17 @@ param(
     [int]$Port
 )
 
+Write-Host "Deploying $Image as $Container on port $Port"
+
 # Stop & remove old container if exists
 docker rm -f $Container 2>$null
 
 # Pull latest image
 docker pull $Image
 
-# Run new container
-docker run -d --name $Container -p $Port:80 $Image
+# Run new container safely
+$DockerArgs = @("run", "-d", "--name", $Container, "-p", "$Port:80", $Image)
+Start-Process docker -ArgumentList $DockerArgs -NoNewWindow -Wait
 
 # Test container
 try {
